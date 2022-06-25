651a5f1aa231478d7df9f01163fc94afda0af6c8
jaybaba8085 <103635623+jaybaba8085@users.noreply.github.com>
2022-06-25 20:30:37 +0530

    Create Nestedloop.java

---
+++ Nestedloop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    for(int i=2;i<5;i++){
+	        System.out.println(i);
+	        for(int j=1;j<2;j++){
+	            System.out.println(j);
+	        }
+	    }
+	}    
+}
