3bebb10dd0dbb9c815e0a47dbd3cffec547ac533
jaybaba8085 <103635623+jaybaba8085@users.noreply.github.com>
2022-06-26 17:55:38 +0530

    Update and rename Prime number Between 1 to 100 in java to Prime-number-Between-1to100in.java

---
+++ Prime-number-Between-1to100in.java
@@ -0,0 +1,21 @@
+class Main 
+{ 
+    public static void main(String args[]) 
+    {     
+        
+         
+     for(int i=1;i<=100;i++)
+       { 
+         int count = 0;
+            for(int j=1;j<=i;j++)
+            {
+             if(i%j==0)
+               count = count + 1;
+            }
+        if(count==2)
+        System.out.println(i);
+       }
+    
+     
+    }
+}
