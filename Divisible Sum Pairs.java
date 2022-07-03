69932fbc37dd7a25add741d0acf56984c19e9cec
jaybaba8085 <103635623+jaybaba8085@users.noreply.github.com>
2022-07-03 17:16:14 +0530

    Create Divisible Sum Pairs.java

---
+++ Divisible Sum Pairs.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int k = sc.nextInt();
+      int arr[] = new int[n]; 
+      for(int i=0;i<n;i++)
+        { 
+          arr[i] =sc.nextInt();
+        }
+      int freq[] = new int[k];
+      for(int i=0;i<n;i++)
+        {
+          freq[arr[i]%k]++;
+        }
+        int ans = (freq[0]*(freq[0]-1))/2;
+      for(int i=1;i<=k/2 && i!=k-1;i++)
+        {
+         ans += freq[i]*freq[k-i];
+        }
+      if(k%2==0)
+       {
+        ans += (freq[k/2]*(freq[k/2]-1))/2;
+       }
+       System.out.print(ans);      
+  	}
+}
