aa6005b6de7dcfdecb5f1f3607ddf4024f48cb25
Sharginjay Patel <103635623+jaybaba8085@users.noreply.github.com>
2022-07-23 20:29:41 +0530

    Update No 'x'  .java

--- RECURSION/No 'x'  .java
+++ RECURSION/No 'x'  .java
@@ -25,3 +25,33 @@ public class Main
     return noX(str,start+1);
   }
 }
+         //Method 2
+
+
+//import java.util.*;
+// import java.lang.*;
+// import java.io.*;
+
+// public class Main
+// {
+//     public static void main (String[] args) throws java.lang.Exception
+//     {
+//         //your code here
+//       Scanner sc = new Scanner(System.in);
+//       String str = sc.next();    
+//       System.out.println(noX(str));
+//     }
+//   public static String noX(String str){
+   
+//     if(str.length()<=0)
+//     {
+//       return "";
+//     }
+//     if(str.charAt(0)!='x'){
+
+//       return str.charAt(0) + noX(str.substring(1));
+
+//     }
+//     return noX(str.substring(1));
+//   }
+// }
